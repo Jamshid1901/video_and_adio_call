1d0a4ab0d0f4fd61492001fbc31d3472a8aebeca
jamshidmirzamaxmudov <jamshimirzamxmudov3@gmail.com>
2023-03-08 18:35:28 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/video_and_adio_call/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.video_and_adio_call
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final AgoraClient client = AgoraClient(
+    agoraConnectionData: AgoraConnectionData(
+      appId: "457e3f501bba48279f5c31ac8a5b5f67",
+      channelName: "test",
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  void initAgora() async {
+    await client.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            AgoraVideoViewer(client: client),
+            AgoraVideoButtons(client: client),
+          ],
+        ),
+      ),
+    );
+  }
+}
